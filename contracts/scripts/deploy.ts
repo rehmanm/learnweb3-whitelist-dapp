50e5b390de1adc59c6778e0782f4e7483b74d9d5
Muhammad Abdul Rehman <muhammadrehman@rocketinnovationstudio.ca>
2022-06-28 11:53:29 -0400

    Code complete without UI

---
+++ contracts/scripts/deploy.ts
@@ -0,0 +1,37 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+import { ethers } from 'hardhat';
+
+import {
+  Whitelist,
+  Whitelist__factory
+} from '../typechain';
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const Whitelist_Factory: Whitelist__factory = await ethers.getContractFactory(
+    "Whitelist"
+  );
+  const whitelist: Whitelist = await Whitelist_Factory.deploy(10);
+
+  await whitelist.deployed();
+
+  console.log("Greeter deployed to:", whitelist.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
